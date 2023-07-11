72ec978e16b366775d0f7660925b4f9dc43a7413
Soujanya-Ghosh <soujanyaghosh66@gmail.com>
2023-07-11 17:48:25 +0530

    My first commit

---
+++ src/inheritence2/CurrentAccount.java
@@ -0,0 +1,36 @@
+package inheritence2;
+
+public class CurrentAccount extends BankAccount{
+    private double overDraftLimit;
+    public CurrentAccount(){}
+
+    public CurrentAccount(String accountNumber, double balance, double overDraftLimit) {
+        super(accountNumber, balance);
+        this.overDraftLimit = overDraftLimit;
+    }
+
+    public double getOverDraftLimit() {
+        return overDraftLimit;
+    }
+
+    public void setOverDraftLimit(double overDraftLimit) {
+        this.overDraftLimit = overDraftLimit;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if(amount <= balance + overDraftLimit){
+            System.out.println("out of limit");
+        }else{
+            balance = balance - amount;
+        }
+       // super.withdraw(amount);
+    }
+
+    @Override
+    public String toString() {
+        return "CurrentAccount{" +
+                "overDraftLimit=" + overDraftLimit +
+                '}';
+    }
+}
