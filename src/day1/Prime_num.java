9c4879dd9407ea2fcbf72d6ca7ab3027e56aeea4
Soujanya-Ghosh <soujanyaghosh66@gmail.com>
2023-07-12 01:28:13 +0530

    My first commit

---
+++ src/day1/Prime_num.java
@@ -0,0 +1,19 @@
+package day1;
+import java.util.Scanner;
+public class Prime_num {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int  prime = 0;
+        for(int i = 1; i <= num ; i++){
+            if(num % i == 0){
+                prime ++;
+            }
+            } if(prime > 2){
+            System.out.println("It is not prime");
+        }else{
+            System.out.println("It is  prime");
+        }
+        }
+    }
+
