b51c8c44091368df2eb5458cb62acdbc627bd395
Soujanya Ghosh <soujanyaghosh66@gmail.com>
2023-08-22 12:48:14 +0530

    My first commit

--- src/Array/MoverZero2.java
+++ src/Array/MoverZero2.java
@@ -3,18 +3,26 @@ package Array;
 public class MoverZero2 {
     public static void main(String[] args) {
         int[] array = {1, 0, 2, 5, 0, 4, 0};
-        int temp;
-        int j = 0;
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] != 0 && array[j] == 0) {
-                temp=array[i];
-                array[i]=array[j];
-                array[j]=temp;
-            }
-            if(array[j]!=0){
-                j++;
-            }
+        moveAllZeros(array);
+        for(int i=0;i< array.length;i++){
             System.out.println(array[i]);
         }
     }
-}
+        static int temp;
+
+         static void moveAllZeros(int[] array){
+            int j = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] != 0 && array[j] == 0) {
+                    temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+                if (array[j] != 0) {
+                    j++;
+                }
+
+            }
+        }
+    }
+
